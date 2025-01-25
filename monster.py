814d9cbfa7f068e666cb7bffd2e76bc72366df95
Michael <michael_dube@mymail.eku.edu>
2025-01-25 16:27:23 -0500

    Started on Monster class.

--- monster.py
+++ monster.py
@@ -1,1 +1,11 @@
 #Put monster superclass stuff here
+import pygame
+class Monster:
+    def __init__(self):
+        
+        self.health
+        self.strength
+        self.speed
+        self.life_steal
+        
+        self.item
